9903e1f93e534c7b04b88672af99b996da7e8577
Terrance  L Smith <terrance@hackerferret.com>
2018-03-25 15:59:55 -0400

    feature(#84):Added tab nav functionality for Run Menu

---
+++ Assets/Scripts/UI/Navigation/TabNavigator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AssemblyCSharp.Assets.Scripts.UI
+{
+    public class TabNavigator : MonoBehaviour
+    {
+        [SerializeField]
+        Canvas[] MenuPanels;
+
+        [SerializeField]
+        NavigatorButton RunMenuNavigation;
+
+        [SerializeField]
+        Canvas RunMenuCanvas;
+
+        [SerializeField]
+        NavigatorButton JumpMenuNavigation;
+
+        [SerializeField]
+        Canvas JumpMenuCanvas;
+
+        [SerializeField]
+        Button DashMenuNavigation;
+
+        [SerializeField]
+        NavigatorButton EnvironmentMenuNavigation;
+
+        private void Start()
+        {
+            //wire up events
+            //var runMenu = MenuPanels.FirstOrDefault(canvas => canvas.name == "RunMenuCanvas");
+            RunMenuNavigation.InitializeArguments(MenuPanels, RunMenuCanvas);
+            JumpMenuNavigation.InitializeArguments(MenuPanels, JumpMenuCanvas);
+        }
+
+        void SetEnabledRunPanelTab(BaseEventData data) { }
+    }
+}
